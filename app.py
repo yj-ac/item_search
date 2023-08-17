ca966594dac235ee45c6571368a3e461d6bc2ed8
yj-ac <63153955+yj-ac@users.noreply.github.com>
2023-08-17 20:45:45 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import streamlit as st
+
+def main():
+    #url = ' http://127.0.0.1:8000/'
+    url = 'https://search_item-1-q2774845.deta.app/'
+    st.title('商品情報検索')
+    select = st.selectbox('入力値',['商品コード','JANコード'])
+    code = st.text_input(f'{select}を入力してください。(商品コードは7桁で)')
+
+    if select == '商品コード':
+        res = requests.get(url+'code/'+code)
+        if res.status_code ==200:
+            res_j = res.json()
+    else:
+        res = requests.get(url+'jan/'+code)
+        if res.status_code == 200:
+            res_j = res.json()
+
+    if code:
+        if res.status_code == 200:
+            st.success('データを取得しました')
+            st.json(res_j)
+        else:
+            st.error('データ取得に失敗しました。正しいコードを入力してください。')
+
+if __name__ == '__main__':
+    main()
