b558e6475713424b1f9fe64e8bf974a4152dcf35
yj-ac <63153955+yj-ac@users.noreply.github.com>
2023-08-20 11:37:50 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import json
 import streamlit as st
 
 def main():
-    #url = ' http://127.0.0.1:8000/'
-    url = 'https://search_item-1-q2774845.deta.app/'
+    url = ' http://127.0.0.1:8000/'
+
     st.title('商品情報検索')
     select = st.selectbox('入力値',['商品コード','JANコード'])
-    code = st.text_input(f'{select}を入力してください。(商品コードは7桁で)')
+    code = st.text_input(f'{select}を入力してください。(商品コードは８桁で)')
 
     if select == '商品コード':
         res = requests.get(url+'code/'+code)
@@ -25,5 +25,5 @@ def main():
         else:
             st.error('データ取得に失敗しました。正しいコードを入力してください。')
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
